596adbf50adb51eac9a64cbce4e0ae2f6a256473
shane6123 <90815651+shane6123@users.noreply.github.com>
2023-03-22 16:25:47 +0530

    fhuhfr

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const { LocalAuth, ChatTypes } = require("whatsapp-web.js");
 const { Client, RemoteAuth } = require('whatsapp-web.js');
 const { MongoStore } = require('wwebjs-mongo');
 const mongoose = require('mongoose');
+const puppeteer = require('puppeteer');
 (async () => {
   const browser = await puppeteer.launch({
     args: ['--no-sandbox', '--disable-setuid-sandbox'],
